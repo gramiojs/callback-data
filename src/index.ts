b3b489f3f5fc2692a2e38f8d8a6dd0d60ed79224
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-05-11 19:17:14 +0300

    chore: improve JSDoc comments

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,53 @@
+/**
+ * @module
+ *
+ * Library for easily manage callback-data
+ */
+
 import { createHash } from "node:crypto";
 import type { AddField, Field, Prettify } from "types";
 
+/**
+ * Class-helper that construct schema and serialize/deserialize with {@link CallbackData.pack} and {@link CallbackData.unpack} methods
+ *
+ * @example
+ * ```typescript
+ * const someData = new CallbackData("example").number("id");
+ *
+ * new Bot()
+ *     .command("start", (context) =>
+ *         context.send("some", {
+ *             reply_markup: new InlineKeyboard().text(
+ *                 "example",
+ *                 someData.pack({
+ *                     id: 1,
+ *                 })
+ *             ),
+ *         })
+ *     )
+ *     .callbackQuery(someData, (context) => {
+ *         context.queryData; // is type-safe
+ *     });
+ * ```
+ */
 export class CallbackData<
 	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 	Schema extends Record<string, any> = Record<never, never>,
 > {
+	/** `id` for identify the CallbackData */
 	id: string;
-
+	/** Schema used for serialize/deserialize with {@link CallbackData.pack} and {@link CallbackData.unpack} methods */
 	schema: Record<string, Field> = {};
 
+	/** Pass the `id` with which you can identify the CallbackData */
 	constructor(id: string) {
 		this.id = createHash("md5").update(id).digest("hex").slice(0, 6);
 	}
 
+	/**
+	 * Add `string` property to schema
+	 * @param key Name of property
+	 */
 	string<Key extends string>(
 		key: Key,
 	): CallbackData<Prettify<Schema & AddField<string, Key>>> {
@@ -23,6 +58,10 @@ export class CallbackData<
 		return this;
 	}
 
+	/**
+	 * Add `number` property to schema
+	 * @param key Name of property
+	 */
 	number<Key extends string>(
 		key: Key,
 	): CallbackData<Prettify<Schema & AddField<number, Key>>> {
@@ -33,6 +72,10 @@ export class CallbackData<
 		return this;
 	}
 
+	/**
+	 * Add `boolean` property to schema
+	 * @param key Name of property
+	 */
 	boolean<Key extends string>(
 		key: Key,
 	): CallbackData<Prettify<Schema & AddField<boolean, Key>>> {
@@ -43,14 +86,39 @@ export class CallbackData<
 		return this;
 	}
 
+	/**
+	 * Method that return {@link RegExp} to match this {@link CallbackData}
+	 */
 	regexp() {
 		return new RegExp(`^${this.id}\\|(.+)$`);
 	}
 
+	/**
+	 * A method for [`serializing`](https://developer.mozilla.org/en-US/docs/Glossary/Serialization) **object data** defined by **schema** into a **string**
+	 * @param data Object defined by schema
+	 *
+	 * @example
+	 * ```ts
+	 * const someData = new CallbackData("example").number("id");
+	 *
+	 * context.send("some", {
+	 * 	reply_markup: new InlineKeyboard().text(
+	 * 		"example",
+	 * 		someData.pack({
+	 * 			id: 1,
+	 * 		}),
+	 * 	),
+	 * });
+	 * ```
+	 */
 	pack(data: Schema) {
 		return `${this.id}|${JSON.stringify(data)}`;
 	}
 
+	/**
+	 * A method for [`deserializing`](https://developer.mozilla.org/en-US/docs/Glossary/Deserialization) data **object** by **schema** from a **string**
+	 * @param data String with data (please check that this string matched by {@link CallbackData.regexp})
+	 */
 	unpack(data: string): Schema {
 		const [id, json] = data.split("|");
 		if (id !== this.id) throw new Error("WIP. id mismatch");
